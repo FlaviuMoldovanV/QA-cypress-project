fe4400c03f5850a6c86988c6f252de2ab1dff452
yourDesiredUsername <69804853+FlaviuMoldovanV@users.noreply.github.com>
2024-02-16 13:06:03 +0200

    feat: add BrokenLinksImages test

--- cypress/POM/demoqaPOM6-9.js
+++ cypress/POM/demoqaPOM6-9.js
@@ -31,3 +31,70 @@ export class Links {
     });
   }
 }
+
+/**
+ * @summary Ensures images load successfully and link accessibility on a webpage using Cypress.
+ * @description
+ * This class includes methods to check for the visibility and proper loading of images,
+ * as well as the accessibility of hyperlinks on a webpage. It leverages Cypress for DOM manipulation
+ * and assertions to ensure that images are not broken and that links lead to accessible URLs.
+ * 
+ * @example
+ * const validator = new BrokenLinksImages();
+ * validator.checkImages('Header Text');
+ * validator.checkLinks('Header Text');
+ */
+
+export class BrokenLinksImages {
+   /**
+   * @summary Checks if images following a specific header are visible and properly loaded.
+   * @description
+   * Targets images immediately following a given header text, checking for their visibility
+   * and loading status by verifying the naturalWidth property. Logs a warning for images
+   * that appear to be broken or have failed to load.
+   * 
+   * @param {string} header - The text of the header preceding the target images.
+   */
+  checkImages(header) {
+    cy.contains(header)
+      .next("img")
+      .should("be.visible")
+      .then(($img) => {
+        // A naturalWidth of 0 or less indicates that the image failed to load.
+        if ($img[0].naturalWidth <= 0) { 
+          Cypress.log({
+            name: "Broken Image",
+            message: "Image didn't load.",
+          });
+        }
+      });
+  }
+
+ /**
+   * @summary Verifies the accessibility of links following a specific header.
+   * @description
+   * Checks the accessibility of hyperlinks by attempting to send a request to the URL
+   * specified in the href attribute. Logs the outcome, indicating whether the link is
+   * accessible or potentially problematic based on the HTTP response status code.
+   * 
+   * @param {string} header - The text of the header associated with the links to validate.
+   */
+  checkLinks(header) {
+    cy.contains(header)
+      .invoke("attr", "href")
+      .then((url) => {
+        cy.request({
+          url: url,
+          failOnStatusCode: false, // Prevents Cypress from failing the test on non-2xx status codes
+        }).then((response) => {
+          if (response.status === 200) {
+            cy.log(`SUCCESS: ${url} is accessible.`);
+          } else {
+            cy.log(
+              `WARNING: ${url} may not be accessible. Status code: ${response.status}`
+            );
+          }
+        });
+      });
+  }
+}
--- cypress/e2e/ElementsTests/demoqaElementsTests6-9.cy.js
+++ cypress/e2e/ElementsTests/demoqaElementsTests6-9.cy.js
@@ -1,6 +1,6 @@
 /// <reference types= "cypress" />
 
-import { Links } from "../../POM/demoqaPOM6-9";
+import { Links, BrokenLinksImages } from "../../POM/demoqaPOM6-9";
 
 describe("DEMOQA Elements Testing 6-9", () => {
   beforeEach(() => {
@@ -8,7 +8,7 @@ describe("DEMOQA Elements Testing 6-9", () => {
     cy.get(".card-up").first().click();
   });
 
-  it("Verifies Link Functionality and API Responses", () => {
+  it.skip("Verifies Link Functionality and API Responses", () => {
     cy.get(".element-group #item-5").first().click();
 
     cy.get("#dynamicLink").invoke("removeAttr", "target").click();
@@ -17,7 +17,16 @@ describe("DEMOQA Elements Testing 6-9", () => {
     let randomApi = new Links();
     randomApi.testAllAPIs();
   });
-  it.skip("Broken Links - Images", () => {});
+
+  it("Ensures Images Load Successfully and Links Are Accessible", () => {
+    cy.get(".element-group #item-6").first().click();
+    let check = new BrokenLinksImages();
+
+    check.checkImages("Valid image");
+    check.checkImages("Broken image");
+    check.checkLinks("Click Here for Valid Link");
+    check.checkLinks("Click Here for Broken Link");
+  });
   it.skip("Upload and Download", () => {});
   it.skip("Dynamic Properties", () => {});
 });
