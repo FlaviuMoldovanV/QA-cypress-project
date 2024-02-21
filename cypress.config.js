f7670947340090bf6014d8f99408345b9bdfcb04
yourDesiredUsername <69804853+FlaviuMoldovanV@users.noreply.github.com>
2024-02-21 11:58:39 +0200

    feat: add Download/Upload test

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,61 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs").promises;
+const path = require("path");
+
+const downloadsFolderPath = "cypress/downloads/"; // Specify your downloads folder path
 
 module.exports = defineConfig({
-  projectId: 'jiz325',
+  projectId: "jiz325",
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("task", {
+        /**
+         * @summary Finds the first file in the downloads directory.
+         * @description 
+         * Searches and retrieves the path of the first file found within a predefined downloads directory. 
+         * By asynchronously reading the contents of the downloads folder and selecting the first file found, 
+         * it allows for dynamic interaction with downloaded files without needing to hardcode file names. 
+         * If no files are found in the directory, it throws an error to signal the absence of downloadable content.
+         * @async
+         * @returns {Promise<string>} The path of the found file.
+         * @throws {Error} if no file is found/ an error occurs while reading the directory.
+         */
+        async findFile() {
+          try {
+            const files = await fs.readdir(downloadsFolderPath); // Reads the directory and assigns the first file name found to 'file'
+            if (!files.length) {
+              throw new Error("No file found");
+            }
+            const file = files[0];
+            const filePath = path.join(downloadsFolderPath, file);
+            return filePath; // Return the full path of the file
+
+          } catch (err) {
+            console.error("Error in findFile:", err.message);
+            throw err;
+          }
+        },
+
+        /**
+         * @summary Deletes a specified file.
+         * @description 
+         * Deletes a specified file from the filesystem. 
+         * This asynchronous operation ensures the file is removed and the filesystem is updated. 
+         * It is primarily used in test cleanup processes to remove temporary files created during test execution.
+         * @async
+         * @param {string} filePath - The path of the file to delete.
+         * @returns {Promise<null>} Resolves with null if the file is successfully deleted.
+         * @throws {Error} If the file could not be deleted.
+         */
+        async deleteFile(filePath) {
+          try {
+            await fs.unlink(filePath); // Delete the file
+            return null;
+          } catch (err) {
+            throw new Error(`Failed to delete file: ${err.message}`);
+          }
+        },
+      });
     },
   },
 });
--- cypress/e2e/ElementsTests/demoqaElementsTests6-9.cy.js
+++ cypress/e2e/ElementsTests/demoqaElementsTests6-9.cy.js
@@ -8,7 +8,7 @@ describe("DEMOQA Elements Testing 6-9", () => {
     cy.get(".card-up").first().click();
   });
 
-  it.skip("Verifies Link Functionality and API Responses", () => {
+  it("Verifies Link Functionality and API Responses", () => {
     cy.get(".element-group #item-5").first().click();
 
     cy.get("#dynamicLink").invoke("removeAttr", "target").click();
@@ -24,9 +24,23 @@ describe("DEMOQA Elements Testing 6-9", () => {
 
     check.checkImages("Valid image");
     check.checkImages("Broken image");
+
     check.checkLinks("Click Here for Valid Link");
     check.checkLinks("Click Here for Broken Link");
   });
-  it.skip("Upload and Download", () => {});
+
+  it("File Upload and Download Automation Test", () => {
+    cy.get(".element-group #item-7").first().click();
+    cy.get("#downloadButton").click();
+
+    // A task is used to find the file's path on the local system
+    cy.task("findFile").then((filePath) => {
+      cy.get("#uploadFile").selectFile(filePath);
+      cy.get("#uploadedFilePath").contains(filePath);
+    //Delete the file from the local system to clean up after the test
+      cy.task("deleteFile", filePath);
+    });
+  });
+
   it.skip("Dynamic Properties", () => {});
 });
